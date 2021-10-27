5e2dff270c511b0196d29ea33a3b4e022fe1046a
Israel M. Rios <israelm.riosjr@gmail.com>
2021-10-27 01:26:03 -0700

    Added lines to server.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const db = require('./db/db.json');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,6 +15,16 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
+
+app.get('/api/notes', (req, res) => res.json(db));
+
+app.post('/api/notes', (req, res) => res.json(db));
+
+app.delete('/api/notes/:id', (req, res) => res.json(db));
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
